d6299a2008eeef1a28a47755e01265352260d10d
Nhat0403 <makarongktk7@gmail.com>
2023-04-13 12:10:57 +0700

    change fetch URL

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [state, setState] = useState([]);
   useEffect(() => {
     const makeAPICall = async() => {
-      const response = await fetch('http://njs-asm-03-be.vercel.app/home')
+      const response = await fetch('https://njs-asm-03-be.vercel.app/home')
       setState(response.message);
     };
     makeAPICall();
