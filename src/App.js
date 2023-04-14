e73efd92ac03b123c0220b79d55604b05533a64d
Nhat0403 <makarongktk7@gmail.com>
2023-04-14 11:05:04 +0700

    adjust API Call

--- src/App.js
+++ src/App.js
@@ -1,16 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
+import axios from 'axios';
+import queryString from 'query-string';
 
 function App() {
   const [state, setState] = useState([]);
+  const baseURL = 'https://njs-asm-03-be.vercel.app/'
+  const axiosClient = axios.create({
+    baseURL: baseURL,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    paramsSerializer: {
+      indexes: null
+    }
+  });
+  const UserAPI = {
+    postLogin: (query) => {
+      const url = 'users/counselor/login' + query;
+      return axiosClient.post(url);
+    }
+  };
   useEffect(() => {
+    // const makeAPICall = async() => {
+    //   const response = await fetch(baseURL + 'home', {
+    //     mode: 'cors',
+    //   });
+    //   const resolve = await response.json();
+    //   setState(resolve.message);
+    //   console.log(resolve);
+    // };
+    // makeAPICall();
+    const params = {
+      email: 'admin@email.com',
+      password: 12345
+    };
+    const query = '?' + queryString.stringify(params);
     const makeAPICall = async() => {
-      const response = await fetch('https://njs-asm-03-be.vercel.app/home', {
+      const response = await fetch(baseURL + 'users/counselor/login' + query, {
         mode: 'cors',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': 'include'
+        },
+        credentials: true
       });
       const resolve = await response.json();
-      setState(resolve.message);
+      console.log(response);
       console.log(resolve);
     };
     makeAPICall();
